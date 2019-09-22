a6d7c73d6b12b6bebe66211ce4ed13de82d5b040
Prof. Wei Qun <weiqun@kmu.ac.kr>
2019-09-22 17:40:45 +0900

    Update AND_OR_NAND.py

--- Python/Perceptron/AND_OR_NAND.py
+++ Python/Perceptron/AND_OR_NAND.py
@@ -21,7 +21,7 @@ def AND(x1, x2):
         return 0
     else: 
         return 1
-    
+print("This is for AND")    
 print(AND(0,0))
 print(AND(0,1))
 print(AND(1,0))
@@ -40,7 +40,7 @@ def NAND(x1, x2):
         return 0
     else:
         return 1
-    
+print("This is for NAND")    
 print(NAND(0,0))
 print(NAND(0,1))
 print(NAND(1,0))
@@ -59,7 +59,7 @@ def OR(x1, x2):
         return 0
     else:
         return 1
-    
+print("This is for OR")    
 print(OR(0,0))
 print(OR(0,1))
 print(OR(1,0))
@@ -73,7 +73,7 @@ def XOR(x1, x2):
     s2 = OR(x1, x2)
     y = AND(s1, s2)
     return y
-print("This is XOR ")
+print("This is for XOR ")
 print(XOR(0,0))
 print(XOR(0,1))
 print(XOR(1,0))
